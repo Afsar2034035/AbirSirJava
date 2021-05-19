ec51fd3be55e18cd8b7c279ec3c868c869d2cd7e
Afsar2034035 <ak0090764@gmail.com>
2021-05-19 23:41:48 +0600

    Math Added

---
+++ math/Test1.java
@@ -0,0 +1,18 @@
+package math;
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-15));
+        System.out.println(Math.sqrt(49));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.PI);
+        System.out.println(Math.log(2.0));
+        System.out.println(Math.max(15,20));
+        System.out.println(Math.min(15,20));
+        System.out.println(Math.ceil(5.5));
+        System.out.println(Math.floor(5.5));
+	}
+
+
+}
