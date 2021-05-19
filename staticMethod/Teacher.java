753d191c763f36c595d5a3d532f6892726f0200a
Afsar2034035 <ak0090764@gmail.com>
2021-05-19 23:45:38 +0600

    Static Method Added

---
+++ staticMethod/Teacher.java
@@ -0,0 +1,15 @@
+package staticMethod;
+
+public class Teacher {
+	
+	void display()
+	{
+		System.out.println("Display Method.");
+	}
+	
+	static void sta()
+	{
+		System.out.println("Static Method.");
+	}
+
+}
---
+++ staticMethod/Test.java
@@ -0,0 +1,14 @@
+package staticMethod;
+
+public class Test {
+
+	
+	public static void main(String[] args)
+	{
+		Teacher a = new Teacher();
+		
+		a.display();
+		
+		Teacher.sta();
+	}
+}
