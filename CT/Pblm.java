07568a1621c4d2b7b4a226e7857fa734a167c7a7
Afsar2034035 <ak0090764@gmail.com>
2021-05-19 23:16:26 +0600

    CT Added

---
+++ CT/Pblm.java
@@ -0,0 +1,18 @@
+package CT;
+import java.util.Scanner;
+
+public class Pblm {
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int a,b,c;
+		
+
+		a = input.nextInt();
+		b = input.nextInt();
+		c = a + b;
+		System.out.println(c);
+	}
+
+}
