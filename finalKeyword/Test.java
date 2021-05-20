8b0265d5df99c6705d9970e0e6c012ecdb830d69
Afsar2034035 <ak0090764@gmail.com>
2021-05-21 01:09:00 +0600

    Final Keyword Added

---
+++ finalKeyword/Test.java
@@ -0,0 +1,14 @@
+package finalKeyword;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		final double PI = 3.1416;
+		
+		//PI = 4.0000;
+		
+		System.out.println(PI);
+	}
+
+}
